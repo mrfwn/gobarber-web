959e34b36f99056fd3f61e310b2c4ac3c1486c82
Mário Wessen <mariowessen@gmail.com>
2020-07-28 20:05:04 -0300

    feature:impoement the auth context and save data in the localstorage

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,52 @@
+import React, { createContext, useCallback, useState } from 'react';
+import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user: object;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: object;
+  signIn(credendials: SignInCredentials): Promise<void>;
+}
+
+export const AuthContext = createContext<AuthContextData>(
+  {} as AuthContextData,
+);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+  const signIn = useCallback(async ({ email, password }: SignInCredentials) => {
+    const {
+      data: { token, user },
+    } = await api.post('sessions', {
+      email,
+      password,
+    });
+
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    setData({ token, user });
+  }, []);
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
